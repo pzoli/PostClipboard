0f7dd102237237351fc782584e992a7c1e7cda2d
Papp Zoltán <papp.zoltan@infokristaly.hu>
2021-10-12 12:24:11 +0200

    Add project files.

---
+++ PostClipboard/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace PostClipboard
+{
+    public class ClipboardContent
+    {
+        public string content { get; set; }
+        public ClipboardContent(string content)
+        {
+            this.content = content;
+        }
+
+    }
+    public static class Clipboard
+    {
+        public static string GetText()
+        {
+            var powershell = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    RedirectStandardOutput = true,
+                    FileName = "powershell",
+                    Arguments = "-command \"Get-Clipboard\""
+                }
+            };
+
+            powershell.Start();
+            string text = powershell.StandardOutput.ReadToEnd();
+            powershell.StandardOutput.Close();
+            powershell.WaitForExit();
+            return text.TrimEnd();
+        }
+    }
+
+
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            ClipboardContent content = new ClipboardContent(Clipboard.GetText());
+            string jsonString = JsonSerializer.Serialize(content);
+            var data = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            using var client = new HttpClient();
+            string url = args[0];
+            string result = null;
+            try
+            {
+                var response = await client.PostAsync(url, data);
+                result = response.Content.ReadAsStringAsync().Result;
+            } catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ PostClipboard/README.md
@@ -0,0 +1,15 @@
+﻿# Project goals
+
+Homework project for share clipboard content.
+
+Ref. project:
+
+https://github.com/pzoli/ClipboardShareMicroservice
+
+# Usage
+
+PostClipboard.exe http://[ip-address]:5000/clipboard/jsonpost
+
+# More media
+
+https://zetcode.com/csharp/httpclient/
